68d226839e20ed2a5c72a4309bbea4a2173b2094
TeePlunder <leon.bergmann23@gmail.com>
2024-03-17 09:02:34 +0100

    fix(db): remove update function

--- api/db.py
+++ api/db.py
@@ -1,7 +1,8 @@
+from typing import List
 from tinydb import TinyDB, Query
 from pydantic import BaseModel
 
-db = TinyDB('db.json')
+db = TinyDB("db.json")
 
 
 class ChipEntry(BaseModel):
@@ -18,18 +19,10 @@ def getAllChips() -> list[ChipEntry]:
 
 
 def addChip(newChip: ChipEntry) -> ChipEntry:
-    db.insert({'chipId': newChip.chipId, 'phoneNumbers': newChip.phoneNumbers})
+    db.insert({"chipId": newChip.chipId, "phoneNumbers": newChip.phoneNumbers})
     return newChip
 
 
-def updateChip(chipId: str, newChipEntry: list[str]) -> ChipEntry:
-    Chip = Query()
-    updatedChip = {'chipId': newChipEntry.chipId,
-                   'phoneNumbers': newChipEntry.phoneNumbers}
-    db.update(updateChip, Chip.chipId == chipId)
-    return updatedChip
-
-
-def removeChip(chipId: str) -> int:
+def removeChip(chipId: str) -> List[int]:
     Chip = Query()
     return db.remove(Chip.chipId == chipId)
